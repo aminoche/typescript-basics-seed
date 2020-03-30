d81a427eeb48e892262150bd2dc44a80dbbf6631
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2020-03-29 22:12:05 -0600

    startin' with typescript

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-console.log('Hello TypeScript!');
+function sumAll(...arr: Array<number>) {
+  console.log(arguments);
+  return arr.reduce((prev, next) => prev + next);
+}
+
+const sum = sumAll(1, 2, 3, 4, 5, 6, 7, 8, 9);
+console.log(sum);
