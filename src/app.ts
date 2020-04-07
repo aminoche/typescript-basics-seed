62b7bb2fd0f717e34997cd06f75ae734d6fb6354
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2020-04-07 13:38:04 -0600

    finished typescript

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,8 @@
-interface SizesInterface {
-  availableSizes: string[];
-}
-
-abstract class Sizes implements SizesInterface {
-  constructor(protected sizes: string[]) {}
-  set availableSizes(sizes: string[]) {
-    this.sizes = sizes;
-  }
-  get availableSizes() {
-    return this.sizes;
-  }
-}
-interface PizzaInterface extends SizesInterface {
-  readonly name: string;
-  toppings: string[];
-  updateSizes(sizes: string[]): void;
-  addTopping(topping: string): void;
-}
-class Pizza extends Sizes implements PizzaInterface {
-  public toppings: string[] = [];
-
-  constructor(readonly name: string, sizes: string[]) {
-    super(sizes);
-  }
-
-  public updateSizes(sizes: string[]) {
-    this.sizes = sizes;
-  }
-
-  public addTopping(topping: string) {
-    this.toppings.push(topping);
+class Coupon {
+  static allowed = ['pepperoni', 'blazing inferno'];
+  static create(percentage: number) {
+    return `PIZZA_RESTAURANT_${percentage}`;
   }
 }
 
-const pizza = new Pizza('pepperoni', ['small', 'medium']);
-pizza.addTopping('cheese');
-pizza.addTopping('pepperoni');
-console.log(pizza.availableSizes);
-pizza.updateSizes(['x-l']);
-console.log(pizza.name);
-console.log(pizza.availableSizes);
-console.log();
-console.log(pizza);
+console.log(Coupon.create(25));
